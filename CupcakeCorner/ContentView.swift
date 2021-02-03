92e3c3214b0e160fcef7621450410bfd3a80f963
Beandip <brando.lugo@gmail.com>
2021-02-03 11:57:45 -0600

    Basic order details added to the view as well as the model for each order

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var order = Order()
 
     var body: some View {
-        Text("Test")
+        NavigationView {
+            Form {
+                //MARK: Cake Type
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes \(order.quantity)")
+                    }
+                }
+                
+                //MARK: Special Requests
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation()){
+                        Text("Any special requests?")
+                    }
+                    
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting, label: {
+                            Text("Add extra frosting")
+                        })
+                        
+                        Toggle(isOn: $order.addSprinkles, label: {
+                            Text("Add extra sprinkles")
+                        })
+                    }
+                }
+                
+                //MARK: Submit
+                Section {
+                    NavigationLink(
+                        destination: AddressView(order: order),
+                        label: {
+                            Text("Delivery Details")
+                        })
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
     }
 }
 
---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,28 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Brando Lugo on 2/3/21.
+//
+
+import Foundation
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocoalte", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    //This resets the values of special requests back to false
+    //if the specialrequest is toggled in the view
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
